addf98728ca09a641c2c1b9c57940b0d2666fd9d
veekeey <meligavictoria@gmail.com>
2022-09-14 12:59:46 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar((res / 10) + '0');
 				_putchar((res % 10) + '0');
 			}
-			else if (res < 10 && j =! 0)
+			else if (res < 10 && j != 0)
 			{
 				_putchar(' ');
 				_putchar((res % 10) + '0');
