1c96e5d20e56ad9141838f6e70109faba8358351
veekeey <meligavictoria@gmail.com>
2022-09-14 03:59:37 -0700

    commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 /**
  * _bs - calculate the absolutevalue of the number from zero.
  * @i: the interger to be converted.
- * Return: absolute value of the input number
+ * Return: absolute value of the input number.
  */
 int _abs(int i)
 {
