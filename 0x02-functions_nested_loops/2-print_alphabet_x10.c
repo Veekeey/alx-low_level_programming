bbf507af6703e5520ce275da1992b4489c33a8cd
veekeey <meligavictoria@gmail.com>
2022-09-14 03:09:02 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alhabet_x10 - prints alphabets 10 times.
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+	}
+}
