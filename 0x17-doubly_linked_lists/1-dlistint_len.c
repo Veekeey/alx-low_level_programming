afdd05bb2678036a3856662896ba48be87bc61b7
veekeey <meligavictoria@gmail.com>
2022-12-08 11:27:48 -0800

    commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - Counts the number of elements in a doubly linked list
+ * @h: The double linked list to count
+ *
+ * Return: Number of elements in the doubly linked list
+ */
+size_t dlistint_len(const dlistint-t *h)
+{
+	int length = 0;
+
+	while (h != NULL)
+	{
+		++length;
+		h = h->next;
+	}
+
+	return (length);
+}
